d2eb1051c3132319555bbd2056b949f45291d4fe
Thalisson-Souza <thalisson.souza@acad.ufsm.br>
2026-05-24 21:49:26 -0300

    test: verify logo presence on homepage

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 def test_deve_abrir_site_tu_berlin():
     driver = webdriver.Chrome()
@@ -6,5 +7,15 @@ def test_deve_abrir_site_tu_berlin():
     driver.get("https://www.tu.berlin/en/")
 
     assert "Technische Universität Berlin" in driver.title
+
+    driver.quit()
+
+
+def test_logo_esta_presente():
+    driver = webdriver.Chrome()
+    driver.get("https://www.tu.berlin/en/")
+
+    logo = driver.find_element(By.CLASS_NAME, "logo")
+    assert logo.is_displayed()
 
     driver.quit()
