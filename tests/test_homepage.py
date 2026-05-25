6339210c3052dd4cb9d828b78c4a3293f90c604a
Thalisson-Souza <thalisson.souza@acad.ufsm.br>
2026-05-24 22:01:34 -0300

    refactor: organize homepage tests with setup and teardown

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,21 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def test_deve_abrir_site_tu_berlin():
-    driver = webdriver.Chrome()
+class TestHomepage:
+    def setup_method(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.tu.berlin/en/")
 
-    driver.get("https://www.tu.berlin/en/")
+    def teardown_method(self):
+        self.driver.quit()
 
-    assert "Technische Universität Berlin" in driver.title
+    def test_deve_abrir_site_tu_berlin(self):
+        assert "Technische Universität Berlin" in self.driver.title
 
-    driver.quit()
-
-
-def test_logo_esta_presente():
-    driver = webdriver.Chrome()
-    driver.get("https://www.tu.berlin/en/")
-
-    logo = driver.find_element(By.CLASS_NAME, "logo")
-    assert logo.is_displayed()
-
-    driver.quit()
+    def test_logo_esta_presente(self):
+        logo = self.driver.find_element(By.CLASS_NAME, "logo")
+        
+        assert logo.is_displayed()
